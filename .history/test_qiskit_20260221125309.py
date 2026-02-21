4bf61a30045414e215715b07a157f9e05dcde2be
Léa-Maude Montpetit <2470673@johnabbottcollege.net>
2026-02-21 13:00:47 -0500

    Generation of a 8-bit unique pad.

--- .history/test_qiskit_20260221125309.py
+++ .history/test_qiskit_20260221125309.py
@@ -1,14 +1,17 @@
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
 
-num_bits = 8
+num_bits = 8 # Length of the pad
 qc = QuantumCircuit(num_bits, num_bits)
 
+# Apply H-gate to all qubits
 for i in range(num_bits):
     qc.h(i)
 
+# Measure all qubits
 qc.measure(range(num_bits), range(num_bits))
 
+# Run simulation
 simulator = AerSimulator()
 compiled_circuit = transpile(qc, simulator)
 job = simulator.run(compiled_circuit, shots=1, memory=True)
